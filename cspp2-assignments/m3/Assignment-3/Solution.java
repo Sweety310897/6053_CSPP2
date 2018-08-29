622d920f1542ccf9e7d79255fafb47aa57654482
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-29 14:11:27 -0600

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -26,7 +26,7 @@ public class Solution {
     /*
 	Need to write the gcd function and print the output.
 	*/
-    public static int gcd(int n1, int n2) {
+    public static int gcd(final int n1, final int n2) {
         int i, temp = 0;
         for (i = 1; i <= n1 && i <= n2; i++) {
             if (n1 % i == 0 && n2 % i == 0) {
