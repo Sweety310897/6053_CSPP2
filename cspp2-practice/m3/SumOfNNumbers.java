ba7e34329d05c4e820db75cebdda8c55c51d9f29
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-29 11:14:46 -0600

    new sum of num

--- cspp2-practice/m3/SumOfNNumbers.java
+++ cspp2-practice/m3/SumOfNNumbers.java
@@ -2,13 +2,12 @@ import java.util.*;
 public class SumOfNNumbers{
 	public static void main(String[] args) {
 		int sum = 0;
-
 		Scanner s = new Scanner(System.in);
 		int num = s.nextInt();
-		while(num>0){
-			int rem = num % 10;
-			sum = sum + rem;
-			num = num / 10;
+		int i;
+		for(i=0;i<=num;i++){
+			sum = sum + i;
+
 		}
 		System.out.println(sum);
 	}
