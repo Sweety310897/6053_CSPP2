2fff8ddea11455f014684f5c88578ffdd962773a
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-29 12:11:04 -0600

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m3/Assignment-2/Solution.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+/*
+	Do not modify this main function.
+	*/
+public class Solution {
+/* Fill the main function to print the number of 7's between 1 to n*/
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);      
+        int n = s.nextInt();
+        int count = 0;
+        int i;
+        for(i = 0;i<n;i++){
+        	if(i%10 == 7){
+        		count++;
+        	}
+        	if(i/10 == 7){
+        		count++;
+        	}
+
+        }
+        System.out.println(count);
+    }
+}
