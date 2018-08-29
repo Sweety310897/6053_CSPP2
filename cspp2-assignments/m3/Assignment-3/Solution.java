f91af1f383df9a77dcbab775eee1db9431edb4ea
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-29 14:08:21 -0600

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m3/Assignment-3/Solution.java
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -18,9 +18,9 @@ public class Solution {
 	*/
     public static int gcd(int n1, int n2)
     {
-        int i,temp=0;
-        for(i =1;i<=n1 && i<=n2;i++){
-            if(n1%i==0 && n2%i==0)
+        int i,temp = 0;
+        for(i = 1; i <= n1 && i <= n2; i++){
+            if(n1 % i == 0 && n2 % i == 0)
             {
                 temp = i;
             }
