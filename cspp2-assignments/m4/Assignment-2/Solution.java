5a097cbffc431f7c3a999da80307fee9044a7dd7
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-30 15:27:24 -0600

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -57,8 +57,7 @@ public final class Solution {
                 }
             System.out.println();
             }
-        }
-        else {
+        } else {
             System.out.println("not possible");
         }
     }
