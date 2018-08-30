85dd5a008a6d1b99dbf07314e871b9ffa75e8af4
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-30 15:08:36 -0600

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -9,45 +9,32 @@ public class Solution {
 		//System.out.println("colsA" + colsA);
 		//int sizeA = s.nextInt();
 		//System.out.println("sizeA" + sizeA);
-
-		
-
-		int[][] sum= new int[rowsA][colsA];
-
-		int[][] a= new int[rowsA][colsA];
-		for(int i = 0; i<rowsA;i++)
-		{
-			for(int j = 0; j<colsA;j++)
-			{
+		int[][] sum = new int[rowsA][colsA];
+		int[][] a = new int[rowsA][colsA];
+		for(int i = 0; i < rowsA; i++) {
+			for(int j = 0; j < colsA; j++) {
 				a[i][j] = s.nextInt();
 			}
 			//System.out.println(a[i] + "a");
 		} 
 		//System.out.println(a);
-
 		int rowsB = s.nextInt();
 		//System.out.println("rowsB" + rowsB);
 		int colsB = s.nextInt();
 		//System.out.println("colsB" + colsB);
 		//int sizeB = s.nextInt();
 		//System.out.println("sizeB" + sizeB);
-
-		int[][] b= new int[rowsB][colsB];
-		for(int i = 0; i<rowsB;i++)
-		{
-			for(int j = 0; j<colsB;j++)
-			{
+		int[][] b = new int[rowsB][colsB];
+		for(int i = 0; i < rowsB; i++) {
+			for(int j = 0; j<colsB; j++) {
 				b[i][j] = s.nextInt();
 			}
 			//System.out.println(b[i] + "b");
 		} 
 		//System.out.println(b);
-		if(rowsA == rowsB && colsA == colsB)
-		{
-			for(int i = 0;i<rowsA;i++)
-		{
-			for(int j=0;j<colsA;j++)
-			{
+		if(rowsA == rowsB && colsA == colsB) {
+			for(int i = 0;i < rowsA; i++) {
+			for(int j = 0;j < colsA; j++) {
 				sum[i][j] = a[i][j] + b[i][j];
 				System.out.print(sum[i][j]);
 				if(j < colsA - 1){
@@ -56,14 +43,9 @@ public class Solution {
 			}
 			System.out.println( );
 		}
-
 		}
-		else
-		{
+		else {
 			System.out.println("not possible");
 		}
-		
-
-		
 	}
 }
