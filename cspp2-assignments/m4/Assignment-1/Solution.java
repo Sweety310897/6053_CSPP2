f16c0d4723b868ba3510763ffcba334483c4a9d1
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-30 18:10:07 -0600

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -3,31 +3,37 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
         //constructor is used
     }
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		int size = s.nextInt();
-		int[] a = new int[size];
-		for (int i = 0; i < size; i++) {
-			a[i] = s.nextInt();
-		}
-		int max = a[0];
-		for (int i = 1; i < size; i++) {
-			//System.out.println(max);
-			if(a[i] > max) {
-				max = a[i];
-			}
-		}
-		System.out.println(max);
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int[] a = new int[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = s.nextInt();
+        }
+        int max = a[0];
+        for (int i = 1; i < size; i++) {
+            //System.out.println(max);
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        System.out.println(max);
+    }
 }
+
+
+
+
+
+
