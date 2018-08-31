8a346b6e6a76cbfb6dc48a72dbedeea469f86011
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-31 15:23:24 -0600

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -2,39 +2,39 @@
 //the decimal value for the given binary string.
 import java.util.Scanner;
 /**
- * Class Solution
+ * Class Solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//Constructor is used.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);
-		System.out.println(reverse);
-	}
-	/**
-	 * Reverse function
-	 *
-	 * @param      s     input string
-	 *
-	 * @return     returns the value
-	 */
-	public static String reverseString(final String s) {
-		String temp = "";
-		for (int i = s.length() - 1; i >= 0; i-- ) {
-			temp = temp + s.charAt(i);
-		}
-		return temp;
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Constructor is used.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /**
+     * Reverse function.
+     *
+     * @param      s     input string
+     *
+     * @return     returns the value
+     */
+    public static String reverseString(final String s) {
+        String temp = "";
+        for (int i = s.length() - 1; i >= 0; i-- ) {
+            temp = temp + s.charAt(i);
+        }
+        return temp;
+    }
 
 }
