899e45bdb671a14612bc3bee335af2de6a145af4
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-31 15:20:00 -0600

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -19,7 +19,7 @@ public final class Solution
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main (final String[] args) {
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
 		String reverse = reverseString(s);	
