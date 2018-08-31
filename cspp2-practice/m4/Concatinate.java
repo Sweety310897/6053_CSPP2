79cbc32180b282c410b8068c4d2e08072ed1a971
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-08-31 14:36:06 -0600

    concat

---
+++ cspp2-practice/m4/Concatinate.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Concatinate
+{
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		String a = s.nextLine();
+		String b = "Hello";
+		System.out.println(b + " " + a + "!");
+
+
+	}
+}
