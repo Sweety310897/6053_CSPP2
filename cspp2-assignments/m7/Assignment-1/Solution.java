03f2743b2d3d8f559cff6e5f98a4fd1c5897a858
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-09-03 18:41:29 -0600

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
  */
 class InputValidator {
 	private String input;
-	
 	/**
 	 * Constructs the object.
 	 *
@@ -22,7 +21,7 @@ class InputValidator {
 		final int z = 6;
 		if (input.length() >= z) {
 			return true;
-		}
+	}
 		else {
 			return false;
 		}
