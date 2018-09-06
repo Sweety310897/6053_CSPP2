5cb983f9f1004722c49a9e8eb7051893c11295f3
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-09-06 10:22:40 -0600

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -285,16 +285,16 @@ public class List {
      */
     public void add(int index,int item) {
          // write the logic
-    	if(index < 0)
-    	{
-    		System.out.println("Negative Index Exception");
-    	}
-    	for(int i = size; i > index;i-- )
-    	{
+    	if(index >= 0) {
+    		for(int i = size; i > index;i-- ) {
     		list[i] = list[i-1];
     	}
     	list[index] = item;
     	size++;
+    	} 	
+    	else {
+    		System.out.println("Negative Index Exception");
+    	}
 
 
     }
