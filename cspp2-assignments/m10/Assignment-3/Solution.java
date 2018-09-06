d020ed6c8e8e08e940af34e014751d491af6fcec
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-09-06 17:35:42 -0600

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-3/Solution.java
+++ cspp2-assignments/m10/Assignment-3/Solution.java
@@ -14,6 +14,10 @@ class Student {
      *
      * @param      name  The name is string.
      */
+    public Student() {
+    //A constructor used to initialize the instance variables
+        // this.name = name;
+    }
     public Student(final String name) {
     //A constructor used to initialize the instance variables
         this.name = name;
