f1111ae9deb9bdb5723d7ed6fa76fbca1e343aa8
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-09-11 14:14:16 -0600

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-1/Solution.java
+++ cspp2-assignments/m15/Assignment-1/Solution.java
@@ -299,6 +299,7 @@ public class Solution {
         }
         catch(Exception e) {
             System.out.println("Index Out Of Bounds Exception");
+            return null;
         }
         // if (start <= 0 || end > size) {
         //     System.out.println("Index Out of Bounds Exception");
