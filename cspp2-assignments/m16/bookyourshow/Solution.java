827085a7c1eebaeae39000a78a216f956541cd81
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-09-13 12:52:39 -0600

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m16/bookyourshow/Solution.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+class BookYourShow {
+    public void addAShow(Show show) {
+
+    }
+    public void bookAShow(String moviename, String date, Patron patron, String[] seats) {
+
+    }
+    public Show getAShow(String moviename, String date) {
+        return null;
+    }
+    public void printTicket(String mobileno, String moviename, String date) {
+
+    }
+    public void showAll() {
+
+    }
+
+}
+class Patron {
+    public Patron(String personname, String mobileno) {
+
+    }
+
+}
+class Show {
+    public Show(String moviename, String date, String[] seats) {
+
+    }
+}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main method to drive program.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        BookYourShow bys = new BookYourShow();
+        Scanner scan = new Scanner(System.in);
+        int testCases = Integer.parseInt(scan.nextLine());
+        System.out.println("trstcases" + testCases);
+        for (int i = 0; i < testCases; i++) {
+            String[] tokens = scan.nextLine().
+                replace("[", "").replace("]", "").split(",");
+            System.out.println(tokens);    
+            String[] check = tokens[0].split(" ");
+            System.out.println(check);
+            switch (check[0]) {
+                case "add":
+                    int k = 2;
+                    String[] seats = new String[tokens.length - 2];
+                    for (int j = 0; j < seats.length; j++) {
+                        seats[j] = tokens[k++];
+                    }
+                    bys.addAShow(new Show(check[1], tokens[1], seats));
+                break;
+
+                case "book":
+                    k = 2 + 2;
+                    seats = new String[tokens.length - 2 - 2];
+                    for (int j = 0; j < seats.length; j++) {
+                        seats[j] = tokens[k++];
+                    }
+                    bys.bookAShow(check[1], tokens[1],
+                        new Patron(tokens[2], tokens[2 + 1]), seats);
+                break;
+
+                case "get":
+                    Show show = bys.getAShow(check[1], tokens[1]);
+                    if (show != null) {
+                       System.out.println(show);
+                    } else {
+                        System.out.println("No show");
+                    }
+                break;
+
+                case "print":
+                    bys.printTicket(check[1], tokens[1], tokens[2]);
+                break;
+
+                case "showAll":
+                    bys.showAll();
+                break;
+
+                default:
+                break;
+            }
+        }
+    }
+}
